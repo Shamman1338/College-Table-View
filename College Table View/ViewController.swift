5227c9c2a6b9c6c4a9d6e01933554ccde91ef0fb
Student <student@bluc02mg0w9fh51.local>
2016-01-20 11:42:58 -0600

    Initial Commit

---
+++ College Table View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  College Table View
+//
+//  Created by Student on 1/20/16.
+//  Copyright © 2016 Shamman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
