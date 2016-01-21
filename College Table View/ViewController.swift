797c72b17ff8664f4b6969524264722d7318567b
Student <student@bluc02mg0w9fh51.local>
2016-01-21 12:42:56 -0600

    Allow entries to be added

--- College Table View/ViewController.swift
+++ College Table View/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var plusButton: UIBarButtonItem!
     
     var colleges = ["Michigan", "High Point", "Elon"]
 
@@ -22,13 +23,37 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return colleges.count
-    }
+}
         func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
-            cell.textLabel?.text = colleges[indexPath.row].name
+            cell.textLabel?.text = colleges[indexPath.row]
             return cell
             
             
+}
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+{
+        if editingStyle == .Delete
+            
+{
+            colleges.removeAtIndex(indexPath.row)
+            tableView.reloadData()
+}
+}
+    @IBAction func onPlusButtonTapped(sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "Add College Here"
         }
-    }
-
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancelAction)
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) -> Void in
+            let cityTextField = alert.textFields![0] as UITextField
+            self.colleges.append(cityTextField.text!)
+            self.tableView.reloadData()
+}
+        alert.addAction(addAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+}
+}
