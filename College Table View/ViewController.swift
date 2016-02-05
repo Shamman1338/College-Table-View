f0cf2b7a6e58458a6094029aa1f4bbf0d19809de
Student <student@bluc02mg0w9fh51.local>
2016-02-05 12:33:04 -0600

    Extra stuff

--- College Table View/ViewController.swift
+++ College Table View/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var editButton: UIBarButtonItem!
     
     var colleges : [College] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,21 +23,24 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         colleges.append(College(name: "High Point", state: "North Carolina", population: 4205, image: UIImage(named: "highpoint")!, webpage: "http://www.highpoint.edu/"))
         colleges.append(College(name: "Elon", state: "North Carolina", population: 6483, image: UIImage(named: "elon")!, webpage: "http://www.elon.edu/home/"))
         colleges.append(College(name: "TCU", state: "Texas", population: 10323, image: UIImage(named: "TCU")!, webpage: "http://www.tcu.edu/"))
- 
     }
+    
     override  func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         tableView.reloadData()
     }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return colleges.count
     }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
-            cell.textLabel?.text = colleges[indexPath.row].name
-            return cell
-            
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        cell.textLabel?.text = colleges[indexPath.row].name
+        return cell
+        
     }
+    
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
     {
         if editingStyle == .Delete
@@ -45,20 +48,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         {
             colleges.removeAtIndex(indexPath.row)
             tableView.reloadData()
+        }
     }
-    }
+    
     @IBAction func onPlusButtonTapped(sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
         alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
-        textField.placeholder = "Add College Here"
-    }
+            textField.placeholder = "Add College Here"
+        }
         let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
         alert.addAction(cancelAction)
         let addAction = UIAlertAction(title: "Add", style: .Default) { (action) -> Void in
             let collegeTextField = alert.textFields![0] as UITextField
             self.colleges.append(College(name: collegeTextField.text!))
             self.tableView.reloadData()
-    }
+        }
         alert.addAction(addAction)
         self.presentViewController(alert, animated: true, completion: nil)
     }
@@ -67,6 +71,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return true
         
     }
+    
     func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
         let college = colleges[sourceIndexPath.row]
         colleges.removeAtIndex(sourceIndexPath.row)
@@ -74,22 +79,23 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     @IBAction func onTappedEditButton(sender: UIBarButtonItem)
-    
+        
     {
         if sender.tag == 0 {
             tableView.editing = true
             sender.tag = 1
-    }
-        
+        }
+            
         else {
             tableView.editing = false
             sender.tag = 0
+        }
     }
-    }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
     {
-            let dvc = segue.destinationViewController as! DetailViewController
-            let index = tableView.indexPathForSelectedRow?.row
-            dvc.college = colleges[index!]
+        let dvc = segue.destinationViewController as! DetailViewController
+        let index = tableView.indexPathForSelectedRow?.row
+        dvc.college = colleges[index!]
     }
-    }
+}
